ce22155a742e6faf2a97a0cf2f18094945bd7b39
Tripamungkas Kartika Adi <tripamungkas.adi@gmail.com>
2021-05-04 16:50:44 +0700

    Fix watcher function
    Bugfixes:
    1. Fix and refactor timer function, merge with
    the watcher function.
    2. Fix whole sentence string input (for
    input which has spaces).

--- cerdas_cermat.go
+++ cerdas_cermat.go
@@ -1,33 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"time"
 )
 
-func timer(timeout int, ch chan<- bool) {
-	time.AfterFunc(time.Duration(timeout)*time.Second, func() {
-		ch <- true
-	})
-}
-
-func watcher(timeout int, ch <-chan bool, quit <-chan bool) {
-	for {
-		select {
-		case <-ch:
-			fmt.Println("Time is up! You have failed!")
-			os.Exit(0)
-		case <-quit:
-			return
-		}
+func watcher(timeout int, quit <-chan bool) {
+	select {
+	case <-time.After(time.Duration(timeout) * time.Second):
+		fmt.Println("Time is up! You have failed!")
+		os.Exit(0)
+	case <-quit:
+		return
 	}
 }
 
 func main() {
-	var ch = make(chan bool)
-	var quit = make(chan bool)
-	var timeout int = 5
+	in := bufio.NewReader(os.Stdin)
+	var quitWatcher = make(chan bool)
+	var timeout int = 3
 	var points int = 0
 	var readyAns string
 	var answer string
@@ -36,7 +29,7 @@ func main() {
 	time.Sleep(2 * time.Second)
 	fmt.Println("Answer each question correctly and win 5 Million Rupiah!")
 	time.Sleep(2 * time.Second)
-	fmt.Println("You have 15 seconds to answer each question, or else you lose!")
+	fmt.Println("You have 3 seconds to answer each question, or else you lose!")
 	time.Sleep(2 * time.Second)
 	fmt.Println("Are you ready? (type Y or N)")
 	fmt.Scanln(&readyAns)
@@ -50,61 +43,61 @@ func main() {
 	fmt.Println("-------------------------------------------------")
 	time.Sleep(2 * time.Second)
 
-	go timer(timeout, ch)
-	go watcher(timeout, ch, quit)
+	go watcher(timeout, quitWatcher)
 	fmt.Println("Question 1 of 3")
 	fmt.Printf("Point : %d\n", points)
 	fmt.Println("Who is the first president of Indonesia? ")
-	fmt.Scan(&answer)
+	answer, _ = in.ReadString('\n')
 
-	if answer == "Ir. Soekarno" || answer == "Ir. Sukarno" || answer == "Soekarno" {
-		quit <- true
+	if answer == "Ir. Soekarno\n" || answer == "Ir. Sukarno\n" || answer == "Soekarno\n" {
+		quitWatcher <- true
 		fmt.Println("Correct Answer!")
 		points += 1
 	} else {
+		quitWatcher <- true
 		fmt.Println("Booo wrong answer!!")
 	}
 
 	fmt.Println("-------------------------------------------------")
 	time.Sleep(2 * time.Second)
 
-	go timer(timeout, ch)
-	go watcher(timeout, ch, quit)
+	go watcher(timeout, quitWatcher)
 	fmt.Println("Question 2 of 3")
 	fmt.Printf("Point : %d\n", points)
 	fmt.Println("Who is the inventor of Go? ")
-	fmt.Scan(&answer)
+	fmt.Scanln(&answer)
 
 	if answer == "China" {
-		quit <- true
+		quitWatcher <- true
 		fmt.Println("Correct Answer!")
 		points += 1
 	} else {
+		quitWatcher <- true
 		fmt.Println("Booo wrong answer!!")
 	}
 
 	fmt.Println("-------------------------------------------------")
 	time.Sleep(2 * time.Second)
 
-	go timer(timeout, ch)
-	go watcher(timeout, ch, quit)
-	fmt.Println("Question 1 of 3")
+	go watcher(timeout, quitWatcher)
+	fmt.Println("Question 3 of 3")
 	fmt.Printf("Point : %d\n", points)
 	fmt.Println("Who is the perpetrator of John F. Kennedy assasination? ")
-	fmt.Scan(&answer)
+	fmt.Scanln(&answer)
 
 	if answer != "" {
-		quit <- true
+		quitWatcher <- true
 		fmt.Println("Correct Answer!")
 		points += 1
 	} else {
+		quitWatcher <- true
 		fmt.Println("Booo wrong answer!!")
 	}
 
 	fmt.Println("-------------------------------------------------")
 	time.Sleep(2 * time.Second)
 
-	fmt.Println("Your point is ", points)
+	fmt.Println("Your point is :", points)
 	if points == 3 {
 		fmt.Println("Congratulations! You win the price! You really are a winner!")
 	} else {
